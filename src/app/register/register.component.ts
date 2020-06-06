7dec2ce2a71d2f9270147c7a91935c6f3bce9fcc
omer5611 <omer5611@gmail.com>
2020-06-07 02:41:40 +0300

    test1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
+//changed 
+// bla bla 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
